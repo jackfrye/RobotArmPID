51480317428187ebf365410888bc73285072851e
mborjas96 <mario.borjas96@gmail.com>
2018-05-30 18:01:48 -0400

    Revert to RobotC 3.x

--- main.c
+++ main.c
@@ -1,62 +1,49 @@
 #pragma config(Sensor, S1,     HTGYRO,              sensorAnalogInactive)
 
+
 #include "constants.h"
-#include "robotcdriversuite/include/hitechnic-gyro.h"
+#include "rdpartyrobotcdr/drivers/hitechnic-gyro.h"
 
 
 task main()
 {
-  displayTextLine(0, "HT Gyro");
-  displayTextLine(1, "Test 1");
-  displayTextLine(5, "Press enter");
-  displayTextLine(6, "to set relative");
-  displayTextLine(7, "heading");
+  nxtDisplayTextLine(0, "HT Gyro");
+  nxtDisplayTextLine(1, "Test 1");
+  nxtDisplayTextLine(5, "Press enter");
+  nxtDisplayTextLine(6, "to set relative");
+  nxtDisplayTextLine(7, "heading");
 
-  sleep(2000);
+  wait1Msec(2000);
   eraseDisplay();
-
-  // Create struct to hold sensor data
-  tHTGYRO gyroSensor;
-
-  // Initialise and configure struct and port
-  initSensor(&gyroSensor, S1);
-
   time1[T1] = 0;
-  while(true)
-  {
+  while(true) {
     if (time1[T1] > 1000) {
       eraseDisplay();
-      displayTextLine(1, "Resetting");
-      displayTextLine(1, "offset");
-      sleep(500);
+      nxtDisplayTextLine(1, "Resetting");
+      nxtDisplayTextLine(1, "offset");
+      wait1Msec(500);
 
       // Start the calibration and display the offset
-      sensorCalibrate(&gyroSensor);
-
-      displayTextLine(2, "Offset: %f", gyroSensor.offset);
-      playSound(soundBlip);
-      while(bSoundActive) sleep(1);
+      nxtDisplayTextLine(2, "Offset: %f", HTGYROstartCal(HTGYRO));
+      PlaySound(soundBlip);
+      while(bSoundActive) EndTimeSlice();
       time1[T1] = 0;
     }
 
-    while(!getXbuttonValue(xButtonEnter))
-    {
+    while(nNxtButtonPressed != kEnterButton) {
       eraseDisplay();
 
-      displayTextLine(1, "Reading");
-
-      // Read the current rotational speed
-      readSensor(&gyroSensor);
-
+      nxtDisplayTextLine(1, "Reading");
       // Read the current calibration offset and display it
-      displayTextLine(2, "Offset: %4f", gyroSensor.offset);
+      nxtDisplayTextLine(2, "Offset: %4f", HTGYROreadCal(HTGYRO));
 
-      displayClearTextLine(4);
+      nxtDisplayClearTextLine(4);
       // Read the current rotational speed and display it
-      displayTextLine(4, "Gyro:   %4f", gyroSensor.rotation);
-      displayTextLine(6, "Press enter");
-      displayTextLine(7, "to recalibrate");
-      sleep(100);
+      nxtDisplayTextLine(4, "Gyro:   %4f", HTGYROreadRot(HTGYRO));
+      nxtDisplayTextLine(6, "Press enter");
+      nxtDisplayTextLine(7, "to recalibrate");
+      wait1Msec(100);
     }
   }
+
 }
