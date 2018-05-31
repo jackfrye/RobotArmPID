255cafcdff5cec672f4b643161a9611e04217f2e
jackfrye <jack.frye@comcast.net>
2018-05-30 21:13:54 -0400

    Cleanup and add logic for ultrasonic sensor

--- main.c
+++ main.c
@@ -1,49 +1,149 @@
 #pragma config(Sensor, S1,     HTGYRO,              sensorAnalogInactive)
+#pragma config(Sensor, S2,     sonarSensor,         sensorSONAR)
 
 
 #include "constants.h"
 #include "rdpartyrobotcdr/drivers/hitechnic-gyro.h"
 
+/*
+ * Debug Modes
+ *		0 - off
+ *		1 - test ultrasonic sensor
+ *		2 - test HiTechnic Gyro Sensor
+ */
+const int DEBUG = 2;
+
+
+
+float init_gyro()
+{
+    float gyro_offset = HTGYROstartCal(HTGYRO);
+    wait1Msec(100);
+
+    return gyro_offset;
+}
+
+/*
+ * Display measurements from ultrasonic sensor
+ */
+void test_ultrasonic_sensor()
+{
+	while(true)
+	{
+		 float sonar_distance_l;
+
+		  eraseDisplay();
+    	wait1Msec(50);
+
+  		sonar_distance_l = SensorValue[sonarSensor];
+
+    	nxtDisplayTextLine(1, "Sonar: %3.2f", sonar_distance_l);
+    	wait1Msec(50);
+  }
+}
+
+/*
+ * Re-initialize the gyro and display measurements
+ *
+ */
+void test_gyro_sensor()
+{
+    nxtDisplayTextLine(0, "HT Gyro");
+	  nxtDisplayTextLine(1, "Test 1");
+	  nxtDisplayTextLine(5, "Press enter");
+	  nxtDisplayTextLine(6, "to set relative");
+	  nxtDisplayTextLine(7, "heading");
+
+	  wait1Msec(2000);
+	  eraseDisplay();
+	  time1[T1] = 0;
+	  while(true) {
+	    if (time1[T1] > 1000) {
+	      eraseDisplay();
+	      nxtDisplayTextLine(1, "Resetting");
+	      nxtDisplayTextLine(1, "offset");
+	      wait1Msec(500);
+
+	      // Start the calibration and display the offset
+	      nxtDisplayTextLine(2, "Offset: %f", HTGYROstartCal(HTGYRO));
+	      PlaySound(soundBlip);
+	      while(bSoundActive) EndTimeSlice();
+	      time1[T1] = 0;
+	    }
+
+	    while(nNxtButtonPressed != kEnterButton) {
+	      eraseDisplay();
+
+	      nxtDisplayTextLine(1, "Reading");
+	      // Read the current calibration offset and display it
+	      nxtDisplayTextLine(2, "Offset: %4f", HTGYROreadCal(HTGYRO));
+
+	      nxtDisplayClearTextLine(4);
+	      // Read the current rotational speed and display it
+	      nxtDisplayTextLine(4, "Gyro:   %4f", HTGYROreadRot(HTGYRO));
+	      nxtDisplayTextLine(6, "Press enter");
+	      nxtDisplayTextLine(7, "to recalibrate");
+	      wait1Msec(100);
+	    }
+	  }
+}
+
 
 task main()
 {
-  nxtDisplayTextLine(0, "HT Gyro");
-  nxtDisplayTextLine(1, "Test 1");
-  nxtDisplayTextLine(5, "Press enter");
-  nxtDisplayTextLine(6, "to set relative");
-  nxtDisplayTextLine(7, "heading");
 
-  wait1Msec(2000);
-  eraseDisplay();
-  time1[T1] = 0;
-  while(true) {
-    if (time1[T1] > 1000) {
-      eraseDisplay();
-      nxtDisplayTextLine(1, "Resetting");
-      nxtDisplayTextLine(1, "offset");
-      wait1Msec(500);
+    /*
+     * Constants
+     *    angle_t 				- current angle
+     *		angle_offset 		- angle originally read during calibration
+     *		gyro_offset			- offset calculated at calibration
+     *		sonar_distance	- distance sensed by sonar
+     */
+    float angle_t;
+    float angle_offset;
+    float gyro_offset;
+    float sonar_distance;
 
-      // Start the calibration and display the offset
-      nxtDisplayTextLine(2, "Offset: %f", HTGYROstartCal(HTGYRO));
-      PlaySound(soundBlip);
-      while(bSoundActive) EndTimeSlice();
-      time1[T1] = 0;
+    /*
+     * Ensure motors will float when nothing is applied
+     */
+    bFloatDuringInactiveMotorPWM = true;
+
+
+    /*
+     * Initialize the HiTechnic Gyro Sensor
+     *		initializes gyro_offset
+     */
+    gyro_offset = init_gyro();
+
+    /*
+     * Read angle_offset
+     */
+    angle_offset = HTGYROreadCal(HTGYRO);
+
+
+    while(true)
+    {
+    	/*
+    	 * Read current angular acceleration from HiTechnic Gyro
+    	 *
+    	 */
+    	angle_t = HTGYROreadRot(HTGYRO);
+
+    	if(DEBUG == 1)
+    	{
+    		test_ultrasonic_sensor();
+      }
+      else if(DEBUG == 2)
+      {
+        test_gyro_sensor();
+      }
+
+    	sonar_distance = SensorValue[sonarSensor];
+
+    	nxtDisplayTextLine(1, "Sonar: %3.2f", sonar_distance);
+    	wait1Msec(50);
+
     }
-
-    while(nNxtButtonPressed != kEnterButton) {
-      eraseDisplay();
-
-      nxtDisplayTextLine(1, "Reading");
-      // Read the current calibration offset and display it
-      nxtDisplayTextLine(2, "Offset: %4f", HTGYROreadCal(HTGYRO));
-
-      nxtDisplayClearTextLine(4);
-      // Read the current rotational speed and display it
-      nxtDisplayTextLine(4, "Gyro:   %4f", HTGYROreadRot(HTGYRO));
-      nxtDisplayTextLine(6, "Press enter");
-      nxtDisplayTextLine(7, "to recalibrate");
-      wait1Msec(100);
-    }
-  }
 
 }
