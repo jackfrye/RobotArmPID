12bcf3ce28664bf03dbca2038e69d7bfec21f8fc
bar63 <blasandresrv@hotmail.com>
2018-06-05 21:26:28 -0400

    ideal constants

--- constants.h
+++ constants.h
@@ -2,10 +2,9 @@
 #ifndef __CONSTANTS_H__
 #define __CONSTANTS_H__
 
-const float kp = 1;
-const float ki = 0;
-const float kd = 0;
-
-const float p_f = 65.0;
+const float kp = 8;
+const float ki = 0.002;
+const float kd = 0.0005;
+const float p_f = 100.0;
 
 #endif
